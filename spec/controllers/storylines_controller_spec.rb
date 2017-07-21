bc3c23265da1970ed4e84ef18efc6180b87ecc4e
Riana Ferreira <rianaferreira@gmail.com>
2017-07-21 15:53:16 +1000

    Revised form to use remote option

--- spec/controllers/storylines_controller_spec.rb
+++ spec/controllers/storylines_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe StorylinesController, type: :controller do
   # Read more:
   # http://blog.bigbinary.com/2016/04/19/changes-to-test-controllers-in-rails-5.html
   # https://github.com/rails/rails/pull/18323/
-  
+
   # Focus on the result of the controller action
   # * what cookies are set?
   # * what HTTP code is set?
@@ -20,17 +20,38 @@ RSpec.describe StorylinesController, type: :controller do
         }
       end
 
+      let(:action) { post :create, params: { storyline: params } }
+
+      it "is successful" do
+        action
+        expect(response.code).to eq("302")
+      end
+
+      it "redirects to thing" do
+        action
+        expect(response).to redirect_to(storyline_path(Storyline.last))
+      end
+
       it "creates a new storyline" do
         expect do
-          post storylines_url, params: { storyline: params }
+          action
         end.to change{ Storyline.count }.by(1)
       end
     end
 
     context "with invalid attributes" do
+      let(:params) do
+        {
+          title: "",
+          plot: "",
+          hero: "",
+          location: ""
+        }
+      end
+
       it "does not create a new storyline" do
         expect do
-          post storylines_url, params: { storyline: {} }
+          post :create, params: { storyline: params }
         end.to_not change{ Storyline.count }
       end
     end
